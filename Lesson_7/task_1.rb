71ff6879996c2e870eaee4eccfc35481129cbc68
Ваше имя <you@example.com>
2019-11-12 13:56:07 +0300

    fixed_bugs

--- Lesson_7/task_1.rb
+++ Lesson_7/task_1.rb
@@ -1,30 +1,27 @@
+
 a = [5, 4, 1, 2, 3, 6, 10, 12]
 b = []
 c = [1, 2, 3, 4]
 
-def score_throws(arr)
+def score_throws(radiuses)
   final_score =  0
 
-  if arr.empty?
-    return puts "Final score = 0"
+  return puts "Final score = 0" if radiuses.empty?
+
+  radiuses.each do |throw|
+    final_score += case throw
+                   when 0...5
+                     10
+                   when 5..10
+                     5
+                   else
+                     0
+                   end
   end
 
-  arr.each do |throw|
-    if throw < 5
-      throw = 10
-    elsif throw > 10
-      throw = 0
-    else
-      throw = 5
-    end
-    final_score += throw
-  end
-
-  if arr.all? { |throw| throw < 5  }
-    final_score += 100
-  end
+  final_score += 100 if radiuses.all? { |throw| throw < 5  }
 
   puts "Final score = #{final_score}"
 end
 
-score_throws(c)
+score_throws(a)
